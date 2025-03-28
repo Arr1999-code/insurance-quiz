f50161f04b7183fe716a79e89b912608626b196b
Richard Chen <richardchen5000@gmail.com>
2025-03-28 14:08:41 -0400

    update

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,56 @@ const originalQuizData = [
     correct: "The company, when it issued the policy"
   },
   {
+    question: "A policyowner suffers an injury that renders him incapable of performing one or more important job duties. Any decrease in income resulting from this injury would make him eligible for benefits under which provision?",
+    options: [
+      "Partial disability",
+      "Non Disabling injury",
+      "Presumptive disability",
+      "Flat amount disability"
+    ],
+    answer: "Partial disability"
+  },
+  {
+    question: "Medicaid is a government-funded program designed to provide health care to",
+    options: [
+      "all individuals over the age of 65.",
+      "All individuals who carry Medicare supplemental insurance.",
+      "Anyone who does not have a proper caregiver.",
+      "Low income state resident."
+    ],
+    answer: "Low income state resident."
+  },
+  {
+    question: "Group Disability Income Insurance usually involves",
+    options: [
+      "Higher premiums than individual disability policies.",
+      "Providing benefits for nonoccupational illnesses and injuries.",
+      "A requirement that all employees participate",
+      "Workers who are individually uninsurable because of their high-risk occupations"
+    ],
+    answer: "Providing benefits for nonoccupational illnesses and injuries."
+  },
+  {
+    question: "Utilization review includes all of the following EXCEPT",
+    options: [
+      "Projected date for release of the patient",
+      "Continues analysis of accident-prone individuals",
+      "Cost-effective ways of handling patients with catastrophic illnesses.",
+      "Intermittent analysis of the reasons for the patient to remain in the healthcare facility"
+    ],
+    answer: "Projected date for release of the patient"
+  },
+  {
+    question: "Which of the following types of long-term care benefits would be best suited for a senior citizen who needs supervision, social, and recreational services?",
+    options: [
+      "Respite care",
+      "Adult Day care",
+      "Home health care",
+      "Nursing home care"
+    ],
+    answer: "Adult Day care"
+  },
+  {
     question: "Which of the following types of long-term care benefits would be best suited for a senior citizen who needs supervision, social, and recreational services?",
     options: ["Respite care", "Adult Day care", "Home health care", "Nursing home care"],
     correct: "Adult Day care"
@@ -35,6 +85,76 @@ const originalQuizData = [
     question: "Utilization review includes all of the following EXCEPT:",
     options: ["Projected date for release of the patient", "Continues analysis of accident-prone individuals", "Cost-effective ways of handling patients with catastrophic illnesses", "Intermittent analysis of the reasons for the patient to remain in the healthcare facility"],
     correct: "Projected date for release of the patient"
+  },
+  {
+    question: "According to the Fair Credit Reporting Act, an insurer does NOT need to",
+    options: [
+      "Inform the applicant that an investigation is being conducted.",
+      "Discuss any credit history inconsistencies with the application.",
+      "Inform the applicant about the scope of an investigation.",
+      "Notify the applicant if an application is denied."
+    ],
+    correct: "Discuss any credit history inconsistencies with the application."
+  },
+  {
+    question: "Which of the following is NOT considered advertising?",
+    options: [
+      "A policy illustration.",
+      "An AM Best company rating.",
+      "A direct mailing by an insurer",
+      "An agent’s oral sales presentation"
+    ],
+    correct: "An AM Best company rating."
+  },
+  {
+    question: "How many days after an accident and health policy has been reinstated is there coverage for sickness?",
+    options: ["1 day", "3 days", "5 days", "10 days"],
+    correct: "10 days"
+  },
+  {
+    question: "A Key Person Disability Income Policy pays benefits to the",
+    options: ["Spouse", "Dependent", "Employee", "Employer"],
+    correct: "Employer"
+  },
+  {
+    question: "A 65-year-old employee who works for an employer with 24 employees is disabled on the job. The employee has fully recovered and returned to work. Which health insurance coverage is primary?",
+    options: [
+      "Medicaid",
+      "An individual’s plan",
+      "Workers’ compensation",
+      "His employer’s group plan"
+    ],
+    correct: "Workers’ compensation"
+  },
+  {
+    question: "Under Workers Compensation, injured employees are covered for all the following losses EXCEPT",
+    options: [
+      "Loss of wages",
+      "Pain and suffering",
+      "Medical expenses",
+      "Occupational illness"
+    ],
+    correct: "Pain and suffering"
+  },
+  {
+    question: "Group long-term disability benefit amounts are usually limited to what percentage of the participant’s income?",
+    options: ["40", "60", "80", "100"],
+    correct: "60"
+  },
+  {
+    question: "Under New York’s cybersecurity regulation, multi-factor authentication would NOT include the use of a",
+    options: [
+      "Key card",
+      "Fingerprint scan",
+      "Password or passcode.",
+      "Text message on a smartphone"
+    ],
+    correct: "Text message on a smartphone"
+  },
+  {
+    question: "Insurance plans that are terminated for an entire group, generally allow an extension of benefits to which type of employee?",
+    options: ["Temporary", "Disabled", "Full time", "Laid off"],
+    correct: "Laid off"
   }
 ];
 
